5765477b75a6074bea97758ab593c6ce2bdcd27f
imaleksei <ialexmmo@yandex.ru>
2023-09-14 23:38:37 +0300

    Task 4: test of the new item creation in addressbook with additional methods & classes Задание №4: Сделать всё то же самое, но для контактов Выполнить действия, аналогичные тем, которые вы проделали в заданиях 2-4, но для контактов (а не для групп контактов):
    Записать в рекордере тест для создания нового контакта в адресной книге (можно при этом не указывать, в какую группу его следует поместить, оставить значение по умолчанию, то есть "ни в какую")
    Перевести тест на язык C# и перенести его в среду разработки Microsoft Visual Studio.
    Собрать проект, запустить созданный тест через Test Explorer.
    Выделить вспомогательные методы, чтобы повысить читаемость тестов.
    Создать вспомогательный класс для хранения свойств контакта.
    Использовать этот вспомогательный класс в тестах для инициализации тестовых данных и для передачи этих данных во вспомогательные методы.

--- addressbook-web-tests/addressbook-web-tests/AddressbookData.cs
+++ addressbook-web-tests/addressbook-web-tests/AddressbookData.cs
@@ -12,6 +12,27 @@ namespace WebAddressBookTests
         private string lastName;
         private string middleName = "";
         private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string emailOne = "";
+        private string emailTwo = "";
+        private string emailThree = "";
+        private string homepage = "";
+        private string addressTwo = "";
+        private string homeTwo = "";
+        private string notes = "";
+        private string bday = "-";
+        private string bmonth = "-";
+        private string byear = "";
+        private string aday = "-";
+        private string amonth = "-";
+        private string ayear = "";
+        private string photo = "C:\\xampp\\TestAdditions\\Capture.PNG";
 
         public AddressbookData(string firstName, string lastName)
         {
@@ -63,6 +84,258 @@ namespace WebAddressBookTests
             set
             {
                 nickname = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string Home
+        {
+            get
+            {
+                return home;
+            }
+            set
+            {
+                home = value;
+            }
+        }
+
+        public string Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+            set
+            {
+                mobile = value;
+            }
+        }
+
+        public string Work
+        {
+            get
+            {
+                return work;
+            }
+            set
+            {
+                work = value;
+            }
+        }
+
+        public string Fax
+        {
+            get
+            {
+                return fax;
+            }
+            set
+            {
+                fax = value;
+            }
+        }
+
+        public string EmailOne
+        {
+            get
+            {
+                return emailOne;
+            }
+            set
+            {
+                emailOne = value;
+            }
+        }
+
+        public string EmailTwo
+        {
+            get
+            {
+                return emailTwo;
+            }
+            set
+            {
+                emailTwo = value;
+            }
+        }
+
+        public string EmailThree
+        {
+            get
+            {
+                return emailThree;
+            }
+            set
+            {
+                emailThree = value;
+            }
+        }
+
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set
+            {
+                homepage = value;
+            }
+        }
+
+        public string AddressTwo
+        {
+            get
+            {
+                return addressTwo;
+            }
+            set
+            {
+                addressTwo = value;
+            }
+        }
+
+        public string HomeTwo
+        {
+            get
+            {
+                return homeTwo;
+            }
+            set
+            {
+                homeTwo = value;
+            }
+        }
+
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
+
+        public string Bday
+        {
+            get
+            {
+                return bday;
+            }
+            set
+            {
+                bday = value;
+            }
+        }
+
+        public string Bmonth
+        {
+            get
+            {
+                return bmonth;
+            }
+            set
+            {
+                bmonth = value;
+            }
+        }
+
+        public string Byear
+        {
+            get
+            {
+                return byear;
+            }
+            set
+            {
+                byear = value;
+            }
+        }
+
+        public string Aday
+        {
+            get
+            {
+                return aday;
+            }
+            set
+            {
+                aday = value;
+            }
+        }
+
+        public string Amonth
+        {
+            get
+            {
+                return amonth;
+            }
+            set
+            {
+                amonth = value;
+            }
+        }
+
+        public string Ayear
+        {
+            get
+            {
+                return ayear;
+            }
+            set
+            {
+                ayear = value;
+            }
+        }
+       
+        public string Photo
+        {
+            get
+            {
+                return photo;
+            }
+            set
+            {
+                photo = value;
             }
         }
     }
